c3ea0698928b671ab12fe2e400601e0a931190e2
Escandia Camargo <camargoescandia@yahoo.com>
2017-07-09 00:14:50 +0200

    Add files via upload

---
+++ js/resumeBuilder.js
@@ -0,0 +1,137 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+ // $("#main").append("Escandia");
+
+// var awesomeThoughts = "I am Escandia and I'm awesome"
+// console.log(awesomeThoughts);
+
+// var funThoughts = awesomeThoughts.replace("awesome", "fun");
+// console.log(funThoughts);
+
+// $("#main").append(funThoughts);
+
+
+// var formattedName = HTMLheaderName.replace("%data%", "Escandia");
+// var formattedRole = HTMLheaderRole.replace("%data%", "Front-End developper")
+//
+// $("#header").append(formattedName);
+// $("#header").append(formattedRole);
+//
+// // First instance
+// var bio = {
+//   "name" : "Escandia",
+//   "role": "Front-End",
+//   "contact": "camargoescandia@gmail.com",
+//   "picture": "https://avatars1.githubusercontent.com/u/24845923?v=3&s=460",
+//   "welcomeMessage": "Any girl can be glamorous, all she has to do is write code -Escandia Camargo-",
+//   "skills": "Self-taught, responsible, dynamic and extraordinary attention to details"
+// };
+//
+// $("#main").append(bio.name);
+// $("#main").append(bio.role);
+// $("#main").append(bio.contact);
+// $("#main").append(bio.picture);
+// $("#main").append(bio.welcomeMessage);
+// $("#main").append(bio.skills);
+//
+//
+// // Second instance hidden
+// // var bio = {
+// //   "name": "Escandia"
+// // };
+// //
+// // bio.role= "Front-End";
+// // bio.contact= "camargoescandia@gmail.com";
+// // bio.picture= "https://avatars1.githubusercontent.com/u/24845923?v=3&s=460)";
+// // bio.welcomeMessage= "Any girl can be glamorous, all she has to do is write code -Escandia Camargo-";
+// // bio.skills= "Self-taught, responsible, dynamic and extraordinary attention to details";
+// //
+// // $("#main").append(bio.role);
+// // $("#main").append(bio.contact);
+// // $("#main").append(bio.picture);
+// // $("#main").append(bio.welcomeMessage);
+// // $("#main").append(bio.skills);
+//
+//
+// var work = {};
+//   work.currentPosition = "Front-End";
+//   work.employer = "Front-End";
+//   work.yearsWorked = "5";
+//   work.city= "Madrid";
+//
+//
+// var education = {};
+//   education.University= "University of Montpellier";
+//   education.yearsAtSchool = "4";
+//   education.schoolCity = "Montpellier, France";
+//
+// $("#main").append(work["currentPosition"]);
+// $("#main").append(work["employer"]);
+// $("#main").append(work["yearsWorked"]);
+// $("#main").append(work["city"]);
+//
+// $("#main").append(education.University);
+// $("#main").append(education.yearsAtSchool);
+// $("#main").append(education.schoolCity);
+//
+//
+// var education2 = {
+//   "schools" :[
+//     {
+//       "name": "University Montpellier",
+//       "cities": "Montpellier",
+//       "majors": "TICS",
+//       "minors": "TICS",
+//       "moocs": ["Python", "JS"]
+//     },
+//     {
+//       "name": "University Sete",
+//       "cities": "Sete",
+//       "majors": "TICS",
+//       "minors": "TICS",
+//       "moocs": ["Python", "JS"]
+//     }
+//   ]
+// }
+
+// var work = {
+//   "job" : "ASSISTANT AT DIRECTION DEPARTMENT OF H&E",
+//   "date" : "2017",
+//   "Company": "Tecopysa",
+//   "description" : "Assistance in project management at Department of hydraulics and environment."
+// };
+//
+// var projects = {
+//       "title" : "EUROPEAN FUNDS WEB PROJECT",
+//       "date": "2017",
+//       "description": "Website development of European funds intended to finance provinces in Spain"
+//     },
+//
+//
+// $("#main").append(work.position);
+// $("#main").append(work.date);
+// $("#main").append(work.Company);
+// $("#main").append(work.description);
+// $("#main").append(projects.title);
+// $("#main").append(projects.date);
+// $("#main").append(projects.description);
+
+// if (bio.skills > 0) {
+//   $("#header").append(bio.skills);
+// }
+// var formattedSkill = skills.replace("%data%", bio.skills[0]);
+// $("#skills").append(formattedSkill);
+
+// for (var jobs in work.job){
+//   $("#workExperience").append(HTMLworkStart);
+// }
+//
+// var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+// var formattedTitle = HTMLworkEmployer.replace("%data%", work.jobs[job].title);
+// var formattedEmployerTitle = formattedEmployer + formattedTitle;
+//
+// $(".work-entry:last").append(
+//   formattedEmployerTitle
+// );
+// }
